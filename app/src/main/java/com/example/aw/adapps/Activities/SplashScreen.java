b3290c97d7a354aadea1fd37394389c30e944da3
eshakom <eshakom@gmail.com>
2016-11-20 21:44:23 +0700

    flashscreen

---
+++ app/src/main/java/com/example/aw/adapps/Activities/SplashScreen.java
@@ -0,0 +1,41 @@
+package com.example.aw.adapps.Activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.aw.adapps.MainActivity;
+import com.example.aw.adapps.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int Splash = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+
+            }
+        }, Splash);
+
+    }
+}
