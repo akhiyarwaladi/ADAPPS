8c66cddf31834e24abc81a6cdd13f2d6df586c67
eshakom <eshakom@gmail.com>
2016-11-20 22:38:26 +0700

    flashscreena

---
+++ app/src/main/java/com/example/aw/adapps/Activities/MenuUtaman.java
@@ -0,0 +1,15 @@
+package com.example.aw.adapps.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aw.adapps.R;
+
+public class MenuUtaman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menuutama);
+    }
+}
